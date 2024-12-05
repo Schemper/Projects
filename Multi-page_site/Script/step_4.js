b4a07f0e6e5708dac7793ecaf8c092139e973b8c
John Toye <victortoye009@gmail.com>
2024-12-05 20:48:06 +0100

    Dynamically generated the HTML for each addOn and calculated the total

--- Multi-page_site/Script/step_4.js
+++ Multi-page_site/Script/step_4.js
@@ -7,62 +7,87 @@ function renderPage(selectedPlan, selectedAddOns) {
 
   const capitalizedDuration = selectedPlan.duration.charAt(0).toUpperCase() + selectedPlan.duration.slice(1).toLowerCase();
 
-  const html = `
-  <h1 class="main__header">
-    Finishing up
-  </h1>
-  <p class="main__desc">
-    Double-check everything looks OK before confirming.
-  </p>
+  const total = calculateTotal (selectedPlan, selectedAddOns)
 
-  <div class="summary">
-    <div class="summary__block summary__duration">
-      <div class="summary__duration--layout">
-        
-        ${selectedPlan.planName} (${capitalizedDuration})
-        
-        <a class="summary__duration-change" href="Step_2.html">
-          Change
-        </a>
+  const formattedDuration = selectedPlan.duration.slice(0, -2).toLowerCase();
+
+  const html = `
+    <h1 class="main__header">
+      Finishing up
+    </h1>
+    <p class="main__desc">
+      Double-check everything looks OK before confirming.
+    </p>
+
+    <div class="summary">
+      <div class="summary__block summary__duration">
+        <div class="summary__duration--layout">
+          
+          ${selectedPlan.planName} (${capitalizedDuration})
+          
+          <a class="summary__duration-change" href="Step_2.html">
+            Change
+          </a>
+        </div>
+          
+        <div class="summary__duration--layout summary__duration-price">$${selectedPlan.cost}${selectedPlan.cycle}</div>
       </div>
-        
-      <div class="summary__duration--layout summary__duration-price">$${selectedPlan.cost}${selectedPlan.cycle}</div>
+      <div class="summary__block addOn__container">
+        ${selectedAddOnsHTML(selectedAddOns, selectedPlan.duration)}
+      </div>
+    
     </div>
-    <div class="summary__block js-summary__block">
-      <p class="summary__desc">Online service</p>
-      <span class="summary__additional-fee">+$1/mo</span>
+    <div class="summary__total-card">
+      <p class="summary__desc summary__total-desc">Total (per ${formattedDuration})</p>
+      <span class="summary__total">+$${total}${selectedPlan.cycle}</span>
     </div>
-    <div class="summary__block">
-      <p class="summary__desc">Larger storage</p>
-      <span class="summary__additional-fee">+$2/mo</span>
-    </div>
-  </div>
-  <div class="summary__total-card">
-    <p class="summary__desc summary__total-desc">Total (per-month)</p>
-    <span class="summary__total">+$12/mo</span>
-  </div>
   `
 
   document.querySelector('.js-main').innerHTML = html;
 }
 
-function selectedAddOnsHTML(selectedAddOns) {
+function selectedAddOnsHTML(selectedAddOns, duration) {
+
+  if (!Array.isArray(selectedAddOns) || selectedAddOns.length === 0) {
+    return '<p>No add-ons selected</p>';
+  }
+
   let html = "";
 
   selectedAddOns.forEach((addOn) => {
+
+    const fee = addOn.billing[duration];
+
     html +=`
-      <p class="summary__desc">${addOn.name}</p>
-      <span class="summary__additional-fee">+$1/mo</span>
+      <div class="summary__addOn">
+        <p class="summary__desc">${addOn.name}</p>
+        <span class="summary__additional-fee">+$${fee.fee}${fee.cycle}</span>
+      </div>
     `
   })
 
   return html;
 }
 
+function calculateTotal (selectedPlan, selectedAddOns) {
+  let total = selectedPlan.cost;
+
+  if (Array.isArray(selectedAddOns)) {
+    selectedAddOns.forEach((addOn) => {
+
+      const fee = addOn.billing[selectedPlan.duration];
+       if (fee) total += fee.fee
+    })
+  }
+
+  return total;
+}
+
 function initializePage() {
   const { selectedPlan, selectedAddOns } = getFromStorage();
 
-  renderPage(selectedPlan);
+  renderPage(selectedPlan, selectedAddOns);
+
 }
 
 document.addEventListener('DOMContentLoaded', initializePage)
