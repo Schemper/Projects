f18783555346d992d1a7f3cec911a6109adc0b70
John Toye <victortoye009@gmail.com>
2025-04-09 19:36:45 +0100

    added event listeners to reveal submenus when clicked

--- intro-section-with-dropdown-navigation/snap.js
+++ intro-section-with-dropdown-navigation/snap.js
@@ -3,12 +3,29 @@ const navBar = document.querySelector('.js-sidebar');
 const closeIcon = document.querySelector('.js-sidebar__close-icon');
 
 menu.addEventListener('click', () => {
-  console.log("Working")
   navBar.classList.add('sidebar--visible');
 });
 
 
 closeIcon.addEventListener('click', () => {
-  console.log('Clicking')
   navBar.classList.remove('sidebar--visible')
+});
+
+const arrowButton = document.querySelectorAll('.sidebar__menu-arrow');
+
+
+
+arrowButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    const imgElem = button.querySelector('img');
+    const submenu = button.parentElement.querySelector('.sidebar__submenu');
+
+    if (imgElem.src.endsWith('/resources/icon-arrow-down.svg')) {
+      imgElem.src='./resources/icon-arrow-up.svg';
+      submenu.classList.add('submenu--visible');
+    } else {
+      imgElem.src='./resources/icon-arrow-down.svg';
+      submenu.classList.remove('submenu--visible');
+    }
+  });
 });
