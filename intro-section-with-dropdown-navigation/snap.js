b23ee74c6387cd4ae39f5517582394607dcfe020
John Toye <victortoye009@gmail.com>
2025-04-19 00:55:11 +0100

    Edited the function to allow the links open the tooltips trays

--- intro-section-with-dropdown-navigation/snap.js
+++ intro-section-with-dropdown-navigation/snap.js
@@ -6,26 +6,33 @@ menu.addEventListener('click', () => {
   navBar.classList.add('sidebar--visible');
 });
 
-
 closeIcon.addEventListener('click', () => {
-  navBar.classList.remove('sidebar--visible')
+  navBar.classList.remove('sidebar--visible');
 });
 
 const arrowButton = document.querySelectorAll('.sidebar__menu-arrow');
 
-
-
 arrowButton.forEach((button) => {
-  button.addEventListener('click', () => {
-    const imgElem = button.querySelector('img');
-    const submenu = button.parentElement.querySelector('.sidebar__submenu');
+  const imgElem = button.querySelector('img');
+  const submenu = button.parentElement.querySelector('.sidebar__submenu');
+  const link = button.parentElement.querySelector('.sidebar__menu-link');
 
+  const toggleSubmenu = () => {
     if (imgElem.src.endsWith('/resources/icon-arrow-down.svg')) {
-      imgElem.src='./resources/icon-arrow-up.svg';
+      imgElem.src = './resources/icon-arrow-up.svg';
       submenu.classList.add('submenu--visible');
     } else {
-      imgElem.src='./resources/icon-arrow-down.svg';
+      imgElem.src = './resources/icon-arrow-down.svg';
       submenu.classList.remove('submenu--visible');
     }
-  });
+  };
+
+  button.addEventListener('click', toggleSubmenu);
+
+  if (link) {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      toggleSubmenu();
+    });
+  }
 });
